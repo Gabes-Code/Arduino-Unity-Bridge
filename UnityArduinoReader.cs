1dbf8c6932f5070d3a36f3efb43a14cba52b3a6d
Gabriel Huseas <gabescode@protonmail.com>
2025-06-27 20:46:39 -0600

    Add files via upload
    This script runs inside of Unity, monitoring the serialport (COM3 by default), and reads serial data written by Arduino.

---
+++ UnityArduinoReader.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.IO.Ports;
+
+public class ArduinoReader : MonoBehaviour
+{
+    SerialPort serial = new SerialPort("COM3", 9600); // Change COM3 to match your port
+
+    void Start()
+    {
+        serial.Open();
+        serial.ReadTimeout = 100;
+    }
+
+    void Update()
+    {
+        if (serial.IsOpen)
+        {
+            try
+            {
+                string value = serial.ReadLine();
+                Debug.Log("Arduino says: " + value);
+                // You can parse value here and use it in Unity
+            }
+            catch (System.Exception)
+            {
+                // Handle read timeout or bad data
+            }
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        if (serial.IsOpen)
+            serial.Close();
+    }
+}
